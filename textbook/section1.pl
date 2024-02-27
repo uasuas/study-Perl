67b56d1fa0fb20ca514698f9982a3217ad34dc76
usausa <143808163+uasuas@users.noreply.github.com>
2024-02-27 17:50:20 +0900

    [update] substr使用方法

--- textbook/section1.pl
+++ textbook/section1.pl
@@ -4,5 +4,9 @@ use warnings;
 # 文字列関数substr。p32
 # substr 関数、文字列の一部を取り出すために使用。
 # substr（対象の文字列、取り出す部分文字列の開始位置を示す整数値、取り出す部分文字列の長さを示す整数値）
-my $s = substr('Programming Lesson', 0, 7);
-print $s;
+my $text = 'Programming Lesson';
+my $s = substr($text, 0, 7);
+print "$s\n";
+# 12個目を含むところから最後までを抽出。
+my $s1 = substr($text, 12);
+print "$s1\n";
